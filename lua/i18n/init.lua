0dfc94f8ee69ca4258bfd95b74d63525f044e080
Lex <38270410+lei4519@users.noreply.github.com>
2025-11-18 19:46:59 +0800

    Clean

--- lua/i18n/init.lua
+++ lua/i18n/init.lua
@@ -1,15 +1,11 @@
--- local virt_text = require("i18n.virt_text")
-
 local i18n = {}
-
-i18n.client = nil
 
 i18n.setup = function(opts)
 	local hl = vim.api.nvim_set_hl
 
 	hl(0, "@i18n.translation", { link = "Comment" })
 
-	local group = vim.api.nvim_create_augroup("js-i18n", {})
+	local group = vim.api.nvim_create_augroup("i18n", {})
 
 	vim.api.nvim_create_autocmd({
 		"BufEnter",
@@ -20,16 +16,7 @@ i18n.setup = function(opts)
 		pattern = "*.{ts,js,tsx,jsx}",
 		group = group,
 		callback = function(ev)
-			-- if ev.event == "TextChangedI" then
-			-- vim.notify("i18n event triggered: " .. ev.event, vim.log.levels.DEBUG)
-			-- return
-			-- end
-			-- local bufnr = ev.buf
-			-- virt_text.set_extmark(bufnr, )
-			-- i18n.client:update_js_file_handler(bufnr)
-
-			-- local workspace_dir = utils.get_workspace_root(bufnr)
-		end,
+			end
 	})
 end
 
